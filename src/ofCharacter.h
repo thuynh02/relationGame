3876b8d9e34bf86fe10f5f7e5634d6c78225fe28
Kay <kaytran@nyu.edu>
2014-06-20 23:21:09 -0400

    Changing Project Name to 'relationGame'

---
+++ src/ofCharacter.h
@@ -0,0 +1,39 @@
+//
+//  ofCharacter.h
+//  Matchmaker
+//
+//  Created by Emrys on 6/20/14.
+//
+//  #ifndef Matchmaker_ofCharacter_h
+//  #define Matchmaker_ofCharacter_h
+//
+//  #endif
+
+#ifndef _OF_CHARACTER
+#define _OF_CHARACTER
+
+#include "ofMain.h"
+
+class ofCharacter {
+    
+public:
+    
+    // Public Methods
+    void update();
+    void draw();
+    
+    // Public Variables
+    float x;      // position
+    float y;
+    float speedY; // speed and direction
+    float speedX;
+    int dim;      // size
+    
+    // Costructor
+    ofBall();
+    
+private:
+    
+};
+
+#endif
