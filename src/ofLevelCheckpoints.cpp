7a62254342a6042cc4871a6e39a4a272ab527d3f
Tony Huynh <th1142@nyu.edu>
2014-06-21 23:13:31 -0400

    Merged with latest changes

--- src/ofLevelCheckpoints.cpp
+++ src/ofLevelCheckpoints.cpp
@@ -13,21 +13,6 @@
 ofLevelCheckpoints::ofLevelCheckpoints( int currentLevel, std::string coordinatesFilePath )
 :level(currentLevel)
 {
-    
-    /*
-     std::string s = "scott>=tiger>=mushroom";
-     std::string delimiter = ">=";
-     
-     size_t pos = 0;
-     std::string token;
-     while ((pos = s.find(delimiter)) != std::string::npos) {
-     token = s.substr(0, pos);
-     std::cout << token << std::endl;
-     s.erase(0, pos + delimiter.length());
-     }
-     std::cout << s << std::endl;
-    */
-    
     std::ifstream ifs( coordinatesFilePath.c_str() );
     std::string delimiter = ",";
     size_t pos = 0;
